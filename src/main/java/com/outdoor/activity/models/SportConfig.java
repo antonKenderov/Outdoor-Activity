85a5bdd9e91d2a02acead8533b488befb7308947
AntonKenderov <shikagamingbg@gmail.com>
2026-06-24 17:11:02 +0300

    Created SportConfig class for the config file and SportConfigWrapper class for getting the sport name.

---
+++ src/main/java/com/outdoor/activity/models/SportConfig.java
@@ -0,0 +1,34 @@
+package com.outdoor.activity.models;
+
+public class SportConfig {
+    private String name;
+    private double minTemperature;
+    private double maxTemperature;
+    private double maxWindSpeed;
+    private int maxRainProbability;
+    private boolean requiresDaylight;
+    private int minDurationHours;
+
+    public SportConfig() {}
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public double getMinTemperature() { return minTemperature; }
+    public void setMinTemperature(double minTemperature) { this.minTemperature = minTemperature; }
+
+    public double getMaxTemperature() { return maxTemperature; }
+    public void setMaxTemperature(double maxTemperature) { this.maxTemperature = maxTemperature; }
+
+    public double getMaxWindSpeed() { return maxWindSpeed; }
+    public void setMaxWindSpeed(double maxWindSpeed) { this.maxWindSpeed = maxWindSpeed; }
+
+    public int getMaxRainProbability() { return maxRainProbability; }
+    public void setMaxRainProbability(int maxRainProbability) { this.maxRainProbability = maxRainProbability; }
+
+    public boolean isRequiresDaylight() { return requiresDaylight; }
+    public void setRequiresDaylight(boolean requiresDaylight) { this.requiresDaylight = requiresDaylight; }
+
+    public int getMinDurationHours() { return minDurationHours; }
+    public void setMinDurationHours(int minDurationHours) { this.minDurationHours = minDurationHours; }
+}
---
+++ src/main/java/com/outdoor/activity/models/SportsConfigWrapper.java
@@ -0,0 +1,10 @@
+package com.outdoor.activity.models;
+
+import java.util.List;
+
+public class SportsConfigWrapper {
+    private List<SportConfig> sports;
+
+    public List<SportConfig> getSports() { return sports; }
+    public void setSports(List<SportConfig> sports) { this.sports = sports; }
+}
