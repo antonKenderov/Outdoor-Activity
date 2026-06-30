a934a3e01d4551f9cd704a3d13efb5e7f24bd477
AntonKenderov <shikagamingbg@gmail.com>
2026-06-30 16:37:14 +0300

    Created basic email service to send emails.

---
+++ src/main/java/com/outdoor/activity/services/EmailService.java
@@ -0,0 +1,30 @@
+package com.outdoor.activity.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmail(String toEmail,
+                          String subject,
+                          String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+
+        mailSender.send(message);
+    }
+}
