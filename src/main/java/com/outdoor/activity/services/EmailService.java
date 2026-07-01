01f6f92b65662501177a50371bc22ad50000b7d9
AntonKenderov <shikagamingbg@gmail.com>
2026-07-01 11:08:11 +0300

    Switched to MimeMessages for sending HTML email response.

--- src/main/java/com/outdoor/activity/services/EmailService.java
+++ src/main/java/com/outdoor/activity/services/EmailService.java
@@ -1,8 +1,10 @@
 package com.outdoor.activity.services;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,12 +20,14 @@ public class EmailService {
 
     public void sendEmail(String toEmail,
                           String subject,
-                          String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(fromEmail);
-        message.setTo(toEmail);
-        message.setText(body);
-        message.setSubject(subject);
+                          String body) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+        helper.setTo(toEmail);
+        helper.setFrom(fromEmail);
+        helper.setSubject(subject);
+        helper.setText(body, true);
 
         mailSender.send(message);
     }
