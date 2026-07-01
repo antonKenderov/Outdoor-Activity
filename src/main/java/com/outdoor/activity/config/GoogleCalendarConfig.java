f195d2d570a052a7527574b903ce1aa47e7a3ded
AntonKenderov <shikagamingbg@gmail.com>
2026-07-01 22:01:38 +0300

    Fixed config file.

--- src/main/java/com/outdoor/activity/config/GoogleCalendarConfig.java
+++ src/main/java/com/outdoor/activity/config/GoogleCalendarConfig.java
@@ -1,32 +1,55 @@
 package com.outdoor.activity.config;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.List;
 
 @Configuration
 public class GoogleCalendarConfig {
 
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final List<String> SCOPES = List.of(CalendarScopes.CALENDAR);
+
     @Bean
     public Calendar googleCalendar() throws IOException, GeneralSecurityException {
-        GoogleCredentials credentials = GoogleCredentials
-                .fromStream(getClass().getResourceAsStream("/credentials.json"))
-                .createScoped(List.of(CalendarScopes.CALENDAR));
+        final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        final GsonFactory jsonFactory = GsonFactory.getDefaultInstance();
 
+        InputStream in = getClass().getResourceAsStream("/credentials.json");
+        if (in == null) {
+            throw new FileNotFoundException("Файлът credentials.json не е намерен в resources!");
+        }
 
-        return new Calendar.Builder(
-                GoogleNetHttpTransport.newTrustedTransport(),
-                GsonFactory.getDefaultInstance(),
-                new HttpCredentialsAdapter(credentials))
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(jsonFactory, new InputStreamReader(in));
+        
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                httpTransport, jsonFactory, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(-1).build();
+        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+
+        return new Calendar.Builder(httpTransport, jsonFactory, credential)
                 .setApplicationName("OutdoorActivity")
                 .build();
     }
