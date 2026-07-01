3ecca1d7aeaa6b98efa53f6bd14a76799705efba
AntonKenderov <shikagamingbg@gmail.com>
2026-07-01 22:38:52 +0300

    Removed comment.

--- src/main/java/com/outdoor/activity/utils/SportEvaluator.java
+++ src/main/java/com/outdoor/activity/utils/SportEvaluator.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
-//Може да се промени
 @Component
 public class SportEvaluator {
     private static final DateTimeFormatter WINDOW_TIME =
